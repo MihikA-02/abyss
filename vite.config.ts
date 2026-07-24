24b73f1229382a9a3c519263b1a22e9f3f418863
MIHIKA <ahirwarmihika@gmail.com>
2026-07-25 01:35:26 +0530

    nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
     tanstackStart({
-      server: { entry: "server", preset: "vercel" }, // 👈 add this
+      server: { entry: "server" },
     }),
+    nitro(),
     viteReact(),
   ],
   resolve: {
